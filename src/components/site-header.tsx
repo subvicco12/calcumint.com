8e18608a472447c9e99def443dd5ba717d257760
subvicco12 <subvicco12@gmail.com>
2026-09-14 13:15:51 +0530

    B5: add Business workspace navigation

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -7,6 +7,7 @@ export function SiteHeader() {
         <Link className="brand" href="/">Calcu<span>Mint</span></Link>
         <nav className="nav" aria-label="Primary navigation">
           <Link className="desktop-link" href="/calculators">Calculators</Link>
+          <Link className="desktop-link" href="/business">Business</Link>
           <Link href="/pricing">Pricing</Link>
           <Link href="/account">Account</Link>
         </nav>
