d54158e562d56b674ec2b975dd4199bfba7d763c
subvicco12 <subvicco12@gmail.com>
2026-09-14 12:46:00 +0530

    B3: load account UI styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
+import "./account.css";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
 import { siteConfig } from "@/lib/site";
