8d8b285bc2f16d5ad616355241b2a65de8132417
subvicco12 <subvicco12@gmail.com>
2026-09-17 23:38:04 +0530

    Finance: register loan prepayment calculator

--- src/calculators/registry.ts
+++ src/calculators/registry.ts
@@ -5,6 +5,7 @@ import { compoundInterestCalculator } from "./finance/compound-interest";
 import { loanPaymentCalculator } from "./finance/loan-payment";
 import { loanAnalysisCalculator } from "./finance/loan-analysis";
 import { loanRefinanceCalculator } from "./finance/loan-refinance";
+import { loanPrepaymentCalculator } from "./finance/loan-prepayment";
 import { termDepositCalculator } from "./finance/term-deposit";
 import { recurringDepositCalculator } from "./finance/recurring-deposit";
 import { sipCalculator } from "./finance/sip";
@@ -17,7 +18,7 @@ type RegistryCalculator = CalculatorDefinition<unknown, unknown>;
 function eraseCalculatorTypes<TInput,TOutput>(definition:CalculatorDefinition<TInput,TOutput>):RegistryCalculator{return definition as unknown as RegistryCalculator;}
 const definitions: readonly RegistryCalculator[] = [
   eraseCalculatorTypes(percentageCalculator), eraseCalculatorTypes(unitConversionCalculator), eraseCalculatorTypes(compoundInterestCalculator), eraseCalculatorTypes(loanPaymentCalculator),
-  eraseCalculatorTypes(loanAnalysisCalculator), eraseCalculatorTypes(loanRefinanceCalculator), eraseCalculatorTypes(termDepositCalculator), eraseCalculatorTypes(recurringDepositCalculator), eraseCalculatorTypes(sipCalculator), eraseCalculatorTypes(stepUpSipCalculator), eraseCalculatorTypes(investmentMathCalculator), eraseCalculatorTypes(swpCalculator),
+  eraseCalculatorTypes(loanAnalysisCalculator), eraseCalculatorTypes(loanRefinanceCalculator), eraseCalculatorTypes(loanPrepaymentCalculator), eraseCalculatorTypes(termDepositCalculator), eraseCalculatorTypes(recurringDepositCalculator), eraseCalculatorTypes(sipCalculator), eraseCalculatorTypes(stepUpSipCalculator), eraseCalculatorTypes(investmentMathCalculator), eraseCalculatorTypes(swpCalculator),
   ...launchDefinitions.map(eraseCalculatorTypes)
 ];
 const bySlug=new Map<string,RegistryCalculator>(); const byId=new Map<string,RegistryCalculator>();
