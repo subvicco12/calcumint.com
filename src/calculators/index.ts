dd92dc8390e40df65761dd37ae2479fe3dbc3cb3
subvicco12 <subvicco12@gmail.com>
2026-09-18 00:50:38 +0530

    Engine: export scenario comparison foundation

--- src/calculators/index.ts
+++ src/calculators/index.ts
@@ -2,6 +2,8 @@ export { runCalculator } from "./engine";
 export { calculatorRegistry } from "./registry";
 export { goalSeek } from "./reverse";
 export type { GoalSeekOptions, GoalSeekResult } from "./reverse";
+export { compareScenarios } from "./scenarios";
+export type { Scenario, ScenarioComparison, ScenarioMetric, ScenarioMetricComparison, ScenarioResult } from "./scenarios";
 export { convertUnit, getUnitFamily, supportedUnits } from "./units";
 export { roundTo } from "./precision";
 export { percentageCalculator } from "./core/percentage";
